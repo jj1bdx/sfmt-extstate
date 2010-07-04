e2f012a98077c63cb10652a0643415b706b3bd89
Kenji Rikitake <kenji.rikitake@acm.org>
2010-07-04 11:42:56 +0859

    simplified test cases, now only checks gen_rand_array(), init_gen_rand(), and init_by_array()

--- test.c
+++ test.c
@@ -272,254 +272,13 @@ void paramdump(void) {
 
 void function_tests(void) {
 
-    w128_t a, b, c, d, r;
-    int shift;
     w128_t testtable[N], array[(10000 / 4)];
     uint32_t *testtable32, *array32;
     int i;
+    uint32_t ini[4] = {0x1234, 0x5678, 0x9abc, 0xdef0};
     
     testtable32 = &testtable[0].u[0];
     array32 = &array[0].u[0];
-
-    printf("rshift128()\n");
-    a.u[0] = 3440181298;
-    a.u[1] = 1564997079;
-    a.u[2] = 1510669302;
-    a.u[3] = 2930277156;
-    shift = 1;
-    rshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 3796268453;
-    a.u[1] = 423124208;
-    a.u[2] = 2143818589;
-    a.u[3] = 3827219408;
-    shift = 2;
-    rshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 2674978610;
-    a.u[1] = 1536842514;
-    a.u[2] = 2027035537;
-    a.u[3] = 2534897563;
-    shift = 3;
-    rshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 545368292;
-    a.u[1] = 1489013321;
-    a.u[2] = 1370534252;
-    a.u[3] = 4231012796;
-    shift = 4;
-    rshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 1764869045;
-    a.u[1] = 824597505;
-    a.u[2] = 862581900;
-    a.u[3] = 2469764249;
-    shift = 5;
-    rshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 359318673;
-    a.u[1] = 116957936;
-    a.u[2] = 3367389672;
-    a.u[3] = 2327178354;
-    shift = 6;
-    rshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 3206507879;
-    a.u[1] = 2378925033;
-    a.u[2] = 1040214787;
-    a.u[3] = 2524778605;
-    shift = 7;
-    rshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    printf("\n");
-
-    printf("lshift128()\n");
-    a.u[0] = 1721379072;
-    a.u[1] = 3897926942;
-    a.u[2] = 1790395498;
-    a.u[3] = 2569178939;
-    shift = 1;
-    lshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 2340259131;
-    a.u[1] = 3144212906;
-    a.u[2] = 2301169789;
-    a.u[3] = 2442885464;
-    shift = 2;
-    lshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 3667880593;
-    a.u[1] = 3935928400;
-    a.u[2] = 2372805237;
-    a.u[3] = 1666397115;
-    shift = 3;
-    lshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 513866770;
-    a.u[1] = 3810869743;
-    a.u[2] = 2147400037;
-    a.u[3] = 2792078025;
-    shift = 4;
-    lshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 3212265810;
-    a.u[1] = 984692259;
-    a.u[2] = 346590253;
-    a.u[3] = 1804179199;
-    shift = 5;
-    lshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 750108141;
-    a.u[1] = 2880257022;
-    a.u[2] = 243310542;
-    a.u[3] = 1869036465;
-    shift = 6;
-    lshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    a.u[0] = 2983949551;
-    a.u[1] = 1931450364;
-    a.u[2] = 4034505847;
-    a.u[3] = 2735030199;
-    shift = 7;
-    lshift128(&b, &a, shift);
-    printf("%d %u %u %u %u %u %u %u %u\n",
-	   shift, a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3]);
-    printf("\n");
-
-    printf("do_recursion()\n");
-    a.u[0] = 3440181298;
-    a.u[1] = 1564997079;
-    a.u[2] = 1510669302;
-    a.u[3] = 2930277156;
-    b.u[0] = 3796268453;
-    b.u[1] = 423124208;
-    b.u[2] = 2143818589;
-    b.u[3] = 3827219408;
-    c.u[0] = 2674978610;
-    c.u[1] = 1536842514;
-    c.u[2] = 2027035537;
-    c.u[3] = 2534897563;
-    d.u[0] = 545368292;
-    d.u[1] = 1489013321;
-    d.u[2] = 1370534252;
-    d.u[3] = 4231012796;
-    do_recursion(&r, &a, &b, &c, &d);
-    printf("%u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u\n",
-	   a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3],
-	   c.u[0], c.u[1], c.u[2], c.u[3],
-	   d.u[0], d.u[1], d.u[2], d.u[3],
-	   r.u[0], r.u[1], r.u[2], r.u[3]);
-    a.u[0] = 1764869045;
-    a.u[1] = 824597505;
-    a.u[2] = 862581900;
-    a.u[3] = 2469764249;
-    b.u[0] = 359318673;
-    b.u[1] = 116957936;
-    b.u[2] = 3367389672;
-    b.u[3] = 2327178354;
-    c.u[0] = 3206507879;
-    c.u[1] = 2378925033;
-    c.u[2] = 1040214787;
-    c.u[3] = 2524778605;
-    d.u[0] = 1417665896;
-    d.u[1] = 964324147;
-    d.u[2] = 2282797708;
-    d.u[3] = 2456269299;
-    do_recursion(&r, &a, &b, &c, &d);
-    printf("%u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u\n",
-	   a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3],
-	   c.u[0], c.u[1], c.u[2], c.u[3],
-	   d.u[0], d.u[1], d.u[2], d.u[3],
-	   r.u[0], r.u[1], r.u[2], r.u[3]);
-    a.u[0] = 2245093271;
-    a.u[1] = 1015729427;
-    a.u[2] = 2694465011;
-    a.u[3] = 3246975184;
-    b.u[0] = 463679346;
-    b.u[1] = 3721104591;
-    b.u[2] = 3475064196;
-    b.u[3] = 856141236;
-    c.u[0] = 3522818941;
-    c.u[1] = 3721533109;
-    c.u[2] = 1954826617;
-    c.u[3] = 1282044024;
-    d.u[0] = 1301863085;
-    d.u[1] = 2669145051;
-    d.u[2] = 4221477354;
-    d.u[3] = 3896016841;
-    do_recursion(&r, &a, &b, &c, &d);
-    printf("%u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u\n",
-	   a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3],
-	   c.u[0], c.u[1], c.u[2], c.u[3],
-	   d.u[0], d.u[1], d.u[2], d.u[3],
-	   r.u[0], r.u[1], r.u[2], r.u[3]);
-    a.u[0] = 462466863;
-    a.u[1] = 1037679449;
-    a.u[2] = 1228140306;
-    a.u[3] = 922298197;
-    b.u[0] = 1872938061;
-    b.u[1] = 3102547608;
-    b.u[2] = 2742766808;
-    b.u[3] = 1888626088;
-    c.u[0] = 157593879;
-    c.u[1] = 1136901695;
-    c.u[2] = 4038377686;
-    c.u[3] = 3572517236;
-    d.u[0] = 2997311961;
-    d.u[1] = 1189931652;
-    d.u[2] = 3981543765;
-    d.u[3] = 2826166703;
-    do_recursion(&r, &a, &b, &c, &d);
-    printf("%u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u %u\n",
-	   a.u[0], a.u[1], a.u[2], a.u[3],
-	   b.u[0], b.u[1], b.u[2], b.u[3],
-	   c.u[0], c.u[1], c.u[2], c.u[3],
-	   d.u[0], d.u[1], d.u[2], d.u[3],
-	   r.u[0], r.u[1], r.u[2], r.u[3]);
-    printf("\n");
-
-    printf("gen_rand_all() = [1...%d]", N32);
-    for (i = 0; i < N32; i++) {
-	testtable32[i] = i + 1;
-    }
-    gen_rand_all(&testtable[0]);
-    for (i = 0; i < N32; i++) {
-	if (0 == i % 4) {
-	    printf("\n");
-	}
-	printf("%u ", testtable32[i]);
-    }
-    printf("\n");
 
     printf("gen_rand_array() = [1...%d] ", N32);
     printf("with 10000 32-bit elements\n");
@@ -565,6 +324,61 @@ void function_tests(void) {
 	    printf("\n");
 	}
 	printf("%u ", array32[i]);
+    }
+    printf("\n");
+
+    printf("\n");
+    printf("init_gen_rand(1234):");
+    init_gen_rand(1234, &testtable[0]);
+    for (i = 0; i < N32; i++) {
+	if (0 == i % 4) {
+	    printf("\n");
+	}
+	printf("%u ", testtable32[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+    printf("init_gen_rand(0)\n");
+    init_gen_rand(0, &testtable[0]);
+    for (i = 0; i < 8; i++) {
+	printf("%u ", testtable32[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+    printf("init_gen_rand(80273972)\n");
+    init_gen_rand(80273972, &testtable[0]);
+    for (i = 0; i < 8; i++) {
+	printf("%u ", testtable32[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+    printf("init_gen_rand(3647962945)\n");
+    init_gen_rand(3647962945, &testtable[0]);
+    for (i = 0; i < 8; i++) {
+	printf("%u ", testtable32[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+    printf("init_gen_rand(3363231301)\n");
+    init_gen_rand(3363231301, &testtable[0]);
+    for (i = 0; i < 8; i++) {
+	printf("%u ", testtable32[i]);
+    }
+    printf("\n");
+    printf("\n");
+
+    printf("init_by_array(ini, 4)\n");
+    printf("ini[4] = {0x1234, 0x5678, 0x9abc, 0xdef0};");
+    init_by_array(ini, 4, &testtable[0]);
+    for (i = 0; i < N32; i++) {
+	if (0 == i % 4) {
+	    printf("\n");
+	}
+	printf("%u ", testtable32[i]);
     }
     printf("\n");
 
